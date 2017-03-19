21c568c547f4079b9520c9fb9ba08eb4d4d47bbe
Venkatesh Kuppan <venkateshkuppan26@gmail.com>
2017-03-19 18:08:26 +0100

    Python script for sending email alert

---
+++ movement.py
@@ -0,0 +1,29 @@
+import time
+import serial
+import smtplib
+TO = 'venkateshkuppan26@gmail.com'
+GMAIL_USER = 'testarduino22@gmail.com'
+GMAIL_PASS = 'abcd'
+SUBJECT = 'Pet Feeder System Alert!!'
+TEXT = 'PIR sensor detected pet movement, Motor will be turned ON'
+ser = serial.Serial('COM2', 9600)
+def send_email():
+	print("Sending Email")
+	smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo
+	smtpserver.login(GMAIL_USER, GMAIL_PASS)
+	header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+	header = header + '\n' + 'Subject:' + SUBJECT + '\n'
+	print header
+	msg = header + '\n' + TEXT + ' \n\n'
+	smtpserver.sendmail(GMAIL_USER, TO, msg)
+	smtpserver.close()
+
+while True:
+	message = ser.readline()
+	print(message)
+	if message[0] == 'P' :
+		send_email()
+	time.sleep(0.5)
